8168fcae0c3c407878d4fbf12b2254a987182f77
tom2u <tbrodhead@2u.com>
2020-06-17 15:11:43 -0500

    18.2.6

--- README.md
+++ README.md
@@ -10,4 +10,5 @@ Each link is to the state of the webpage at the END of the indicated lesson.
 [18.1.9](https://github.com/tom2u/coding-online-module-18/tree/master/18.1.9)  
 [18.2.4](https://github.com/tom2u/coding-online-module-18/tree/master/18.2.4)  
 [18.2.5](https://github.com/tom2u/coding-online-module-18/tree/master/18.2.5)  
+[18.2.6](https://github.com/tom2u/coding-online-module-18/tree/master/18.2.6)  
 
--- models/Pizza.js
+++ models/Pizza.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require("mongoose");
+const moment = require("moment");
 
 const PizzaSchema = new Schema(
   {
@@ -11,6 +12,8 @@ const PizzaSchema = new Schema(
     createdAt: {
       type: Date,
       default: Date.now,
+      get: (createdAtVal) =>
+        moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
     },
     size: {
       type: String,
@@ -27,6 +30,7 @@ const PizzaSchema = new Schema(
   {
     toJSON: {
       virtuals: true,
+      getters: true,
     },
     id: false,
   }
